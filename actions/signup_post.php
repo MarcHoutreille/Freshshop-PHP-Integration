d0242134dafd7887394022d7dbe5eee5152a2bbd
Marc Houtreille <marc.houtreille@gmail.com>
2021-04-27 16:58:56 +0200

    work on signup

--- actions/signup_post.php
+++ actions/signup_post.php
@@ -1,7 +1,13 @@
 <?php
 
-if ((!empty($_POST['firstname'])) & (!empty($_POST['lastname'])) & (!empty($_POST['email'])) & (!empty($_POST['password'])) & (!empty($_POST['password_confirm']))) {
-    echo "All info are there";
-} else {
-    echo "NO :(";
+function verifySignUp($firstname,$lastname,$email,$password,$password_confirm) {
+
+    if ((!empty($firstname)) & (!empty($lastname)) & (!empty($email)) & (!empty($password)) & (!empty($password_confirm))) {
+        echo "All info are there";
+    } else {
+        echo "NO :(";
+    }
+
 }
+
+verifySignUp($_POST['firstname'],$_POST['lastname'],$_POST['email'],$_POST['password'],$_POST['password_confirm']);
--- classes/userscontr.class.php
+++ classes/userscontr.class.php
@@ -7,5 +7,19 @@ class UsersContr extends Users {
         $this->setUser($email,$firstname,$lastname,$password);
     }
 
+    public function verifySignUp($firstname,$lastname,$email,$password,$password_confirm) {
 
+        if ((!empty($firstname)) & (!empty($lastname)) & (!empty($email)) & (!empty($password)) & (!empty($password_confirm))) {
+            echo "All info are there";
+            if ($password == $password_confirm) {
+                echo "Passwords are matching";
+            } else {
+                echo "Password don't match :(";
+            }
+        } else {
+            echo "NO :(";
+        }
+    
+    }
+    
 }
